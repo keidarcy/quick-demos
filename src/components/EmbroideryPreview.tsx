0e375b57b2e115a2f21f7c4868bca6482ef6fe15
XING YAHAO <keidarcy.1015@gmail.com>
2020-11-14 11:39:37 +0900

    update

---
+++ src/components/EmbroideryPreview.tsx
@@ -0,0 +1,171 @@
+import React, { useReducer } from 'react';
+
+const initState = {
+  text: '',
+  size: 'h1',
+  bgColor: 'bg-light',
+  fontColor: 'text-dark',
+  position: '',
+  image: ''
+};
+
+type ActionType = { field: string; value: string };
+
+const reduer = (state: typeof initState, action: ActionType) => {
+  const newState = { ...state, [action.field]: action.value };
+  localStorage.setItem('SHOWCASES', JSON.stringify(newState));
+  return newState;
+};
+
+function EmbroideryPreview() {
+  const [state, dispatch] = useReducer(
+    reduer,
+    localStorage.getItem('SHOWCASES')
+      ? JSON.parse(localStorage.getItem('SHOWCASES') as string)
+      : initState
+  );
+  const bgColors = [
+    'bg-primary',
+    'bg-secondary',
+    'bg-success',
+    'bg-danger',
+    'bg-warning',
+    'bg-info',
+    'bg-light',
+    'bg-dark'
+  ];
+  const fontColors = [
+    'text-primary',
+    'text-secondary',
+    'text-success',
+    'text-danger',
+    'text-warning',
+    'text-info',
+    'text-light',
+    'text-dark'
+  ];
+
+  const sizes = ['h1', 'h2', 'h3', 'h4', 'h5'];
+
+  const postions = ['flex-row', 'flex-row-reverse', 'flex-column', 'flex-column-reverse'];
+
+  const images = [
+    'https://lh6.googleusercontent.com/CmLF2OtWLmvHxxbpfbByAWha60zQuBta_jnYq0VN2e7VNfkoYN0RO2ua3hjFKKBfdP1-C-UdgE36OHpB2DX9bcvO_X8SUEdZVY0VTAvP0eU66RD3CN0mo6h48jsdtd2MdRM0C35euYc',
+    'https://lh4.googleusercontent.com/8Ypdpv7qkZmFeKTMneRC1y3jdvMriv-9jLVcTD5RmXwu9abJrL7j8mQPZhI9gfje8IchKonzzWRAqxq75eMFxIkDCjtopGmkFxI902Y9aPu--qnNBW1BtWSefvjnp_1MkufGQyUxWPo',
+    'https://lh6.googleusercontent.com/hu8GQaI2h69LPnOfnfwhIXteNpomSTnRA5WYVRo914EX5DjQegPPl7GnidN08TEWqWhBAohrGCdIQy7eSG5pNbio2cDSQJyycnPlYWp6avPAKwXv3-fgz_JI77gj4YsPhhAOCqdw3sQ',
+    'https://lh4.googleusercontent.com/3tc0pvYNOhkGnO7ZPt029CWjAsByEU74tRcP27-LxaWicT-L-3SugaPBCyOEdBFMcR8ki7JKvdaxx15KC2Q87skZTQEYdo3emLckS2KI6_2OL3tNvwzDe8fTM9356Y8wneeX_OtJFnw',
+    'https://lh3.googleusercontent.com/mSKDKszuD_mgkxnhUjjPVA8inNFphytqOxa98pu4Od5sA3bnPVrcTr0EOIizt5mwpwYVixnV8997utBA7SgwNJmLIYv-Yk_kMsInpIIv93mvWvD6BBPe48sKcTmssqmwTZGduRvMLEk'
+  ];
+
+  const toFullWidth = (value: string) => {
+    if (!value) return value;
+    return String(value).replace(/[!-~]/g, function (all) {
+      return String.fromCharCode(all.charCodeAt(0) + 0xfee0);
+    });
+  };
+
+  return (
+    <>
+      <div className="bg-green-200 h-full my-5">
+        <div className="mx-auto" style={{ width: '30rem' }}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              value={state.text}
+              onChange={(e) => dispatch({ field: 'text', value: e.target.value })}
+            />
+          </div>
+          <label htmlFor="background color">Background Color</label>
+          <div className="d-flex p-2 bd-highlight">
+            {bgColors.map((c) => (
+              <div
+                key={c}
+                className={`card ${c} mx-1`}
+                style={{ width: '1rem', height: '1rem' }}
+                onClick={(e) => dispatch({ field: 'bgColor', value: c })}
+              ></div>
+            ))}
+          </div>
+          <label htmlFor="font color">Font Color</label>
+          <div className="d-flex p-2 bd-highlight">
+            {bgColors.map((c, index) => (
+              <div
+                key={c}
+                className={`card ${c} mx-1`}
+                style={{ width: '1rem', height: '1rem' }}
+                onClick={() => dispatch({ field: 'fontColor', value: fontColors[index] })}
+              ></div>
+            ))}
+          </div>
+          <label htmlFor="font size">Font Size</label>
+          <div className="d-flex p-2 bd-highlight">
+            {sizes.map((s) => (
+              <div
+                key={s}
+                className={`card mx-1`}
+                style={{ width: '2rem', height: '2rem' }}
+                onClick={() => dispatch({ field: 'size', value: s })}
+              >
+                {s}
+              </div>
+            ))}
+          </div>
+          <label htmlFor="position">Position</label>
+          <div className="d-flex p-2 bd-highlight">
+            {postions.map((p) => (
+              <div
+                key={p}
+                className="mx-1 border border-secondary rounded"
+                onClick={() => dispatch({ field: 'position', value: p })}
+              >
+                {p}
+              </div>
+            ))}
+          </div>
+          <label htmlFor="image">Image</label>
+          <div className="d-flex p-2 bd-highlight">
+            {images.map((i) => (
+              <img
+                src={i}
+                key={i}
+                alt={i}
+                className="mx-1 rounded-sm"
+                width="200px"
+                onClick={() => dispatch({ field: 'image', value: i })}
+              />
+            ))}
+          </div>
+          <div
+            className={`container card ${state.bgColor} mb-3 ${state.fontColor}`}
+            style={{ minWidth: '40rem' }}
+          >
+            <div className={`d-flex card-body ${state.size} ${state.position}`}>
+              <p
+                className="card-text font-weight-bold"
+                style={{ fontFamily: "'Pinyon Script', cursive" }}
+              >
+                {toFullWidth(state.text)}
+              </p>
+              <p
+                className="card-text font-weight-bold"
+                style={{ fontFamily: "'Pinyon Script', cursive" }}
+              >
+                {state.text}
+              </p>
+              <p
+                className="card-text"
+                style={{ fontFamily: "'Noto Sans JP', sans-serif" }}
+              >
+                {state.text}
+              </p>
+              {state.image ? <img src={state.image} alt="penggin" width="100px" /> : null}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default EmbroideryPreview;
---
+++ src/components/ShopifyMultipass.tsx
@@ -0,0 +1,219 @@
+import React, { useReducer, useState } from 'react';
+import { Multipass } from 'multipass-js';
+
+enum ACTION_TYPES {
+  CHANGE = 'CHANGE',
+  SUBMIT = 'SUBMIT',
+  TOGGLE_LOADING = 'TOGGLE_LOADING'
+}
+
+const initialState = {
+  domain: 'xxx.myshopify.com',
+  key: '',
+  redirect: '/',
+  data: JSON.stringify(
+    {
+      email: 'aaa@aaa.com',
+      tag_string: 'multipass',
+      first_name: 'TEST',
+      last_name: 'TEST',
+      addresses: [
+        {
+          address1: '123 Oak St',
+          city: 'Ottawa',
+          country: 'Canada',
+          first_name: 'TEST',
+          last_name: 'TEST',
+          phone: '555-1212',
+          province: 'Ontario',
+          zip: '123 ABC',
+          province_code: 'ON',
+          country_code: 'CA',
+          default: true
+        }
+      ]
+    },
+    undefined,
+    2
+  ),
+  url: ''
+};
+
+type State = typeof initialState;
+
+type Action =
+  | { type: ACTION_TYPES.CHANGE; payload: { field: string; value: string } }
+  | { type: ACTION_TYPES.SUBMIT };
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case ACTION_TYPES.CHANGE:
+      return { ...state, [action.payload.field]: action.payload.value };
+    case ACTION_TYPES.SUBMIT: {
+      if (!state.domain || !state.data || !state.key) {
+        alert('全項目は必須です。All fields are required.');
+        return state;
+      } else {
+        let url;
+        if (state.redirect) {
+          url = new Multipass(state.key)
+            .withCustomerData(JSON.parse(state.data))
+            .withDomain(state.domain)
+            .withRedirect(state.redirect)
+            .url();
+        } else {
+          url = new Multipass(state.key)
+            .withCustomerData(JSON.parse(state.data))
+            .withDomain(state.domain)
+            .withRedirect(state.redirect)
+            .url();
+        }
+        return { ...state, url };
+      }
+    }
+    default:
+      return state;
+  }
+};
+
+const t = {
+  en: {
+    domain: 'Shopify Store Domain',
+    redirect: 'redirect',
+    secret: 'Multipass Secret',
+    customer: 'Customer Data',
+    alert: 'Invalid Input Content',
+    email: 'email is required.',
+    button: 'Generate Mutipass URL',
+    doc: 'Shopify multipass documentation'
+  },
+  ja: {
+    domain: 'Shopify ドメイン',
+    redirect: 'リダイレクト',
+    secret: 'マルチパス シークレット',
+    customer: '顧客データ',
+    alert: '入力項目チェックしてください。',
+    email: 'emailは必須。',
+    button: 'マルチパス用のURL生成',
+    doc: 'Shopify マルチパスドキュメント'
+  }
+};
+
+function ShopifyMultipass() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [isJa, setIsJa] = useState(
+    !new URLSearchParams(window.location.search).get('lang')
+  );
+  return (
+    <>
+      <div className="btn-group" role="group" aria-label="Basic example">
+        <button
+          type="button"
+          onClick={() => setIsJa(true)}
+          className={`btn ${isJa ? 'btn-primary' : 'btn-outline-primary'}`}
+        >
+          日本語
+        </button>
+        <button
+          type="button"
+          onClick={() => setIsJa(false)}
+          className={`btn ${isJa ? 'btn-outline-warning' : 'btn-warning'}`}
+        >
+          English
+        </button>
+      </div>
+      <div className="container my-5">
+        <form>
+          <div className="form-group">
+            <label htmlFor="domain">{isJa ? t.ja.domain : t.en.domain}</label>
+            <input
+              type="input"
+              className="form-control"
+              onChange={(e) =>
+                dispatch({
+                  type: ACTION_TYPES.CHANGE,
+                  payload: { field: e.target.id, value: e.target.value }
+                })
+              }
+              value={state.domain}
+              id="domain"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="redirect">{isJa ? t.ja.redirect : t.en.redirect}</label>
+            <input
+              type="input"
+              className="form-control"
+              onChange={(e) =>
+                dispatch({
+                  type: ACTION_TYPES.CHANGE,
+                  payload: { field: e.target.id, value: e.target.value }
+                })
+              }
+              value={state.redirect}
+              id="redirect"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="secret">{isJa ? t.ja.secret : t.en.secret}</label>
+            <input
+              type="input"
+              value={state.key}
+              className="form-control"
+              onChange={(e) =>
+                dispatch({
+                  type: ACTION_TYPES.CHANGE,
+                  payload: { field: e.target.id, value: e.target.value }
+                })
+              }
+              id="key"
+            />
+            <a
+              href="https://shopify.dev/docs/admin-api/rest/reference/plus/multipass"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {isJa ? t.ja.doc : t.en.doc}
+            </a>
+          </div>
+          <div className="form-group">
+            <label htmlFor="customer">{isJa ? t.ja.customer : t.en.customer}</label>
+            <textarea
+              className="form-control"
+              onChange={(e) =>
+                dispatch({
+                  type: ACTION_TYPES.CHANGE,
+                  payload: { field: e.target.id, value: e.target.value }
+                })
+              }
+              id="data"
+              rows={6}
+              value={state.data}
+            ></textarea>
+            <small id="emailHelp" className="form-text text-muted">
+              {isJa ? t.ja.email : t.en.email}
+            </small>
+          </div>
+          <button
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              dispatch({ type: ACTION_TYPES.SUBMIT });
+            }}
+            className="btn btn-primary"
+          >
+            {isJa ? t.ja.button : t.en.button}
+          </button>
+        </form>
+      </div>
+      <div className="container">
+        <p>Muliplepass URL:</p>
+        <a href={state.url} target="_blank" rel="noopener noreferrer">
+          {state.url}
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default ShopifyMultipass;
